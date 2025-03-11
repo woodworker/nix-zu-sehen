0abf6dbc188063ddf72acac74cf2f54fdc96731d
Martin Holzhauer <martin@holzhauer.eu>
2025-03-11 13:26:31 +0100

    Nix für obsidian-cli

---
+++ obsidian-cli.nix
@@ -0,0 +1,33 @@
+{ pkgs, go, lib }:
+
+pkgs.buildGoModule rec {
+  name = "obsidian-cli-${version}";
+  version = "0.1.6"; # replace with the package version
+
+  src = pkgs.fetchFromGitHub {
+    owner = "Yakitrak"; # replace with the GitHub username
+    repo = "obsidian-cli"; # replace with the package repository name
+    rev = "v${version}"; # replace with the package version tag
+    sha256 = "sha256-DmmtvhGp8Ux1WZ2n0ZQe+Yp/5X6DYY/YZmyWAJDcBqU="; # replace with the source code checksum
+  };
+
+  vendorHash = null;#lib.fakeHash;
+
+  # If the package has dependencies outside of Go, list them here
+  # For example:
+  # buildInputs = [ go pkgconfig zlib ];
+
+  # If the package is a command-line tool, you can specify the output binaries
+  # For example:
+  # meta.installable = true;
+  # meta.installable.bin = ["my-go-package"];
+
+  meta = {
+    description = "Interact with Obsidian in the terminal. Open, search, create, update, move and delete notes!";
+    homepage = "https://github.com/Yakitrak/obsidian-cli";
+    license = lib.licenses.mit; # replace with the appropriate license
+    maintainers = [
+      { name = "Martin Holzhauer"; email = "martin@holzhauer.eu"; }
+    ];
+  };
+}
